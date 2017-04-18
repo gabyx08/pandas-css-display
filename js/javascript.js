bc9b6ea2f2e4fb1c4c045f1f4dcdf75240f22831
fbk-rena <gaby.ranarz@gmail.com>
2017-04-18 11:13:34 -0500

    boton restaurar fotos funciona

--- js/javascript.js
+++ js/javascript.js
@@ -8,3 +8,12 @@ function cerrarImagen(){
   var div= this.parentElement;
   div.style.display = "none";
 }
+
+var btnRestaurar = document.getElementById("btnRestaurar");
+btnRestaurar.addEventListener("click", restaurarImagenes);
+
+function restaurarImagenes(){
+  for(var i = 0; i<cerrar.length; i++){
+    cerrar[i].parentElement.style.display = "inline-block";
+  }
+}
