eaab46bbd86bc4e4f14e067e4eb28ff66928197a
fbk-rena <gaby.ranarz@gmail.com>
2017-04-18 15:28:34 -0500

    funcionan botones origen y extincion

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,8 @@
+//cerrar las imagenes al darle click a X
 var cerrar = document.getElementsByClassName("cerrar");
 
 for(var i = 0; i<cerrar.length; i++){
-  cerrar[i].addEventListener("click",cerrarImagen)
+  cerrar[i].addEventListener("click",cerrarImagen);
 }
 
 function cerrarImagen(){
@@ -9,11 +10,40 @@ function cerrarImagen(){
   div.style.display = "none";
 }
 
+//boton resturar imagenes
 var btnRestaurar = document.getElementById("btnRestaurar");
 btnRestaurar.addEventListener("click", restaurarImagenes);
 
 function restaurarImagenes(){
   for(var i = 0; i<cerrar.length; i++){
     cerrar[i].parentElement.style.display = "inline-block";
+  }
+}
+
+//botones (origen y extincion) y eventos
+var btnOrigen = document.getElementById("btnOrigen");
+btnOrigen.addEventListener("click",mostrarOcultarOrigen);
+
+var btnExtincion = document.getElementById("btnExtincion")
+btnExtincion.addEventListener("click",mostrarOcultarExtincion);
+
+//textos
+var txtOrigen = document.getElementById("txtOrigen");
+var txtExtincion = document.getElementById("txtExtincion");
+
+function mostrarOcultarOrigen(){
+  if(txtOrigen.style.visibility == "hidden"){
+    console.log("ocultar")
+    txtOrigen.style.visibility= "visible";
+  }else{
+    console.log("mostrar")
+    txtOrigen.style.visibility= "hidden";
+  }
+}
+function mostrarOcultarExtincion(){
+  if(txtExtincion.style.visibility== "hidden"){
+    txtExtincion.style.visibility= "visible";
+  }else{
+    txtExtincion.style.visibility= "hidden";
   }
 }
